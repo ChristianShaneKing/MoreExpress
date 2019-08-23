72babb3534446de7594e610335671d4d88a3ec27
Christian King <christianshaneking@gmail.com>
2019-08-23 04:36:59 +0000

    modify files in views dir

--- EJSDemo/app.js
+++ EJSDemo/app.js
@@ -5,6 +5,21 @@ app.get("/", function(req, res){
 	res.render("home.ejs");
 });
 
+app.get("/fallinlovewith/:thing", function(req, res){
+	var thing = req.params.thing;
+	res.render("love.ejs", {thingVar: thing});
+});
+
+app.get("/posts", function(req, res){
+	var posts = [
+		{title: "Post 1", author: "Brock"},
+		{title: "I need more pokeballs", author: "Misty"},
+		{title: "Pikachu bit me!", author: "Ash"}
+	];
+	
+	res.render("posts.ejs", {posts: posts});
+});
+
 app.listen(3000, function(){
 	console.log("The server has started!");
 });
